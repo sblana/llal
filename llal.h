7e0e276afe4b43a49b2ceb969633c3a1b5ad295d
sblana <adeline.l.a.brauer@gmail.com>
2025-01-05 08:20:21 +0100

    header update

--- llal.h
+++ llal.h
@@ -1,5 +1,4 @@
 #ifndef LLAL_H
-
 #define LLAL_H
 
 #include <stdlib.h>
@@ -92,6 +91,285 @@ typedef union {
 	struct { uint r, g, b, a; };
 	struct { uint s, t, p, q; };
 } uint4;
+
+
+typedef union {
+	float c[4];
+	float2 v[2];
+	struct { float2 x, y; };
+	struct {
+		float xx, xy;
+		float yx, yy;
+	};
+	struct { float2 r, g; };
+	struct {
+		float rr, rg;
+		float gr, gg;
+	};
+	struct { float2 s, t; };
+	struct {
+		float ss, st;
+		float ts, tt;
+	};
+} float2x2;
+
+typedef union {
+	double c[4];
+	double2 v[2];
+	struct { double2 x, y; };
+	struct {
+		double xx, xy;
+		double yx, yy;
+	};
+	struct { double2 r, g; };
+	struct {
+		double rr, rg;
+		double gr, gg;
+	};
+	struct { double2 s, t; };
+	struct {
+		double ss, st;
+		double ts, tt;
+	};
+} double2x2;
+
+typedef union {
+	int c[4];
+	int2 v[2];
+	struct { int2 x, y; };
+	struct {
+		int xx, xy;
+		int yx, yy;
+	};
+	struct { int2 r, g; };
+	struct {
+		int rr, rg;
+		int gr, gg;
+	};
+	struct { int2 s, t; };
+	struct {
+		int ss, st;
+		int ts, tt;
+	};
+} int2x2;
+
+typedef union {
+	uint c[4];
+	uint2 v[2];
+	struct { uint2 x, y; };
+	struct {
+		uint xx, xy;
+		uint yx, yy;
+	};
+	struct { uint2 r, g; };
+	struct {
+		uint rr, rg;
+		uint gr, gg;
+	};
+	struct { uint2 s, t; };
+	struct {
+		uint ss, st;
+		uint ts, tt;
+	};
+} uint2x2;
+
+
+typedef union {
+	float c[9];
+	float3 v[3];
+	struct { float3 x, y, z; };
+	struct {
+		float xx, xy, xz;
+		float yx, yy, yz;
+		float zx, zy, zz;
+	};
+	struct { float3 r, g, b; };
+	struct {
+		float rr, rg, rb;
+		float gr, gg, gb;
+		float br, bg, bb;
+	};
+	struct { float3 s, t, p; };
+	struct {
+		float ss, st, sp;
+		float ts, tt, tp;
+		float ps, pt, pp;
+	};
+} float3x3;
+
+typedef union {
+	double c[9];
+	double3 v[3];
+	struct { double3 x, y, z; };
+	struct {
+		double xx, xy, xz;
+		double yx, yy, yz;
+		double zx, zy, zz;
+	};
+	struct { double3 r, g, b; };
+	struct {
+		double rr, rg, rb;
+		double gr, gg, gb;
+		double br, bg, bb;
+	};
+	struct { double3 s, t, p; };
+	struct {
+		double ss, st, sp;
+		double ts, tt, tp;
+		double ps, pt, pp;
+	};
+} double3x3;
+
+typedef union {
+	int c[9];
+	int3 v[3];
+	struct { int3 x, y, z; };
+	struct {
+		int xx, xy, xz;
+		int yx, yy, yz;
+		int zx, zy, zz;
+	};
+	struct { int3 r, g, b; };
+	struct {
+		int rr, rg, rb;
+		int gr, gg, gb;
+		int br, bg, bb;
+	};
+	struct { int3 s, t, p; };
+	struct {
+		int ss, st, sp;
+		int ts, tt, tp;
+		int ps, pt, pp;
+	};
+} int3x3;
+
+typedef union {
+	uint c[9];
+	uint3 v[3];
+	struct { uint3 x, y, z; };
+	struct {
+		uint xx, xy, xz;
+		uint yx, yy, yz;
+		uint zx, zy, zz;
+	};
+	struct { uint3 r, g, b; };
+	struct {
+		uint rr, rg, rb;
+		uint gr, gg, gb;
+		uint br, bg, bb;
+	};
+	struct { uint3 s, t, p; };
+	struct {
+		uint ss, st, sp;
+		uint ts, tt, tp;
+		uint ps, pt, pp;
+	};
+} uint3x3;
+
+
+typedef union {
+	float c[16];
+	float4 v[4];
+	struct { float4 x, y, z, w; };
+	struct {
+		float xx, xy, xz, xw;
+		float yx, yy, yz, yw;
+		float zx, zy, zz, zw;
+		float wx, wy, wz, ww;
+	};
+	struct { float4 r, g, b, a; };
+	struct {
+		float rr, rg, rb, ra;
+		float gr, gg, gb, ga;
+		float br, bg, bb, ba;
+		float ar, ag, ab, aa;
+	};
+	struct { float4 s, t, p, q; };
+	struct {
+		float ss, st, sp, sq;
+		float ts, tt, tp, tq;
+		float ps, pt, pp, pq;
+		float qs, qt, qp, qq;
+	};
+} float4x4;
+
+typedef union {
+	double c[16];
+	double4 v[4];
+	struct { double4 x, y, z, w; };
+	struct {
+		double xx, xy, xz, xw;
+		double yx, yy, yz, yw;
+		double zx, zy, zz, zw;
+		double wx, wy, wz, ww;
+	};
+	struct { double4 r, g, b, a; };
+	struct {
+		double rr, rg, rb, ra;
+		double gr, gg, gb, ga;
+		double br, bg, bb, ba;
+		double ar, ag, ab, aa;
+	};
+	struct { double4 s, t, p, q; };
+	struct {
+		double ss, st, sp, sq;
+		double ts, tt, tp, tq;
+		double ps, pt, pp, pq;
+		double qs, qt, qp, qq;
+	};
+} double4x4;
+
+typedef union {
+	int c[16];
+	int4 v[4];
+	struct { int4 x, y, z, w; };
+	struct {
+		int xx, xy, xz, xw;
+		int yx, yy, yz, yw;
+		int zx, zy, zz, zw;
+		int wx, wy, wz, ww;
+	};
+	struct { int4 r, g, b, a; };
+	struct {
+		int rr, rg, rb, ra;
+		int gr, gg, gb, ga;
+		int br, bg, bb, ba;
+		int ar, ag, ab, aa;
+	};
+	struct { int4 s, t, p, q; };
+	struct {
+		int ss, st, sp, sq;
+		int ts, tt, tp, tq;
+		int ps, pt, pp, pq;
+		int qs, qt, qp, qq;
+	};
+} int4x4;
+
+typedef union {
+	uint c[16];
+	uint4 v[4];
+	struct { uint4 x, y, z, w; };
+	struct {
+		uint xx, xy, xz, xw;
+		uint yx, yy, yz, yw;
+		uint zx, zy, zz, zw;
+		uint wx, wy, wz, ww;
+	};
+	struct { uint4 r, g, b, a; };
+	struct {
+		uint rr, rg, rb, ra;
+		uint gr, gg, gb, ga;
+		uint br, bg, bb, ba;
+		uint ar, ag, ab, aa;
+	};
+	struct { uint4 s, t, p, q; };
+	struct {
+		uint ss, st, sp, sq;
+		uint ts, tt, tp, tq;
+		uint ps, pt, pp, pq;
+		uint qs, qt, qp, qq;
+	};
+} uint4x4;
 
 
 float2 f2_add(float2 a, float2 b);
